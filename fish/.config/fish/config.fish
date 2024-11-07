3a66cf2708f66d81c977a58670670973ecabb958
Gray Karegeannes <graykk@gmail.com>
2024-11-06 23:00:44 -0500

    Adding new config for fish, hyprland, kitty, waybar, wofi

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
