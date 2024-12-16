8501f337b3726c79ffaf581ff8a8a20771d98429
Gray Karegeannes <graykk@gmail.com>
2024-12-16 15:04:47 -0500

    Updating Kitty to launch tmux automatically

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,7 +7,18 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #end
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
+set -U fish_user_paths /usr/bin $fish_user_paths
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# cuda
+set -Ux CC /usr/bin/gcc-13
+set -Ux CXX /usr/bin/g++-13
+
+set -Ux PATH /opt/cuda/bin $PATH
+set -Ux LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
+set -U fish_user_paths /opt/cuda/bin $fish_user_paths
+
+
