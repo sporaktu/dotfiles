b0933ce87dfefa7d76f5c4f440e7deacaa343871
Gray Karegeannes <graykk@gmail.com>
2025-04-08 23:29:22 -0400

    Adding ghostty config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,24 +1,13 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+# function fish_greeting
+#    echo "Welcome, $USER!"
+#    echo "System Information:"
+#    uname -a
+# end
+
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 set -U fish_user_paths /usr/bin $fish_user_paths
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-# cuda
-set -Ux CC /usr/bin/gcc-13
-set -Ux CXX /usr/bin/g++-13
-
-set -Ux PATH /opt/cuda/bin $PATH
-set -Ux LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
-set -U fish_user_paths /opt/cuda/bin $fish_user_paths
-
+starship init fish | source
 
