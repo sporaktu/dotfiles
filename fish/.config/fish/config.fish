629cdb82bdf8b0c326bb5394be57bfabe5189d94
Gray Karegeannes <graykk@gmail.com>
2025-09-17 14:08:10 -0400

    Adding more yazi plugins and setting yazi hotkey alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,6 +11,7 @@ set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 set -U fish_user_paths /opt/homebrew/bin/fish $fish_user_paths
 alias localllama="python3 ~/claude_local.py"
+alias y="yazi"
 
 starship init fish | source
 
